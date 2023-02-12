3ccafd566734f13f11553c91ceb07ed117576fea
Renatto Neciosup <renattonl@gmail.com>
2023-02-12 12:02:03 -0500

    test read dirs

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@ const express = require('express');
 const PORT = process.env.APP_PORT || 3000;
 const app = express();
 
+const test = () => {
+  const fs = require('fs');
+  // const path = require('path');
+  const directories = fs.readdirSync( process.env.STORAGE_PATH_LOCAL );
+  return directories;
+}
+
 app.get('/', (req, res) => {
   res.status(200).json({
     status: true,
+    directories: test(),
   });
 });
 
